7507ec4b1f5638a773095cfb1fc62846c0925ffc
mcejalvo <marc.cejalvo@gmail.com>
2019-08-09 10:40:57 +0200

    Rolling Ball Set Up

---
+++ Eppur-Move/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    private int count; 
+    public Text countText;
+
+    
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        
+    }
+    void FixedUpdate ()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+        
+        rb.AddForce(movement * speed, ForceMode.Impulse);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Cosito"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+    }
+}
